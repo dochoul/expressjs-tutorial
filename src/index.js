3837e478027ffcd33a9abc273969261a89619dd9
dochoul@gmail.com <dochoul@gmail.com>
2023-11-06 23:56:52 +0900

    DB connect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
+const mongoose = require("mongoose");
+
+//* Connect MongoDB
+const uri =
+  "mongodb+srv://board:1234@cluster0.22pjovr.mongodb.net/?retryWrites=true&w=majority";
+async function run() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongDB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+run();
+//* (END)Connect MongoDB
 
 const groceriesRoute = require("./routes/groceries");
 const marketsRoute = require("./routes/markets");
