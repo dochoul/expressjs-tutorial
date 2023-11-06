4b7707ede4a8aaa34aaf3b9bc66cd718b7eeb645
dochoul@gmail.com <dochoul@gmail.com>
2023-11-06 22:57:33 +0900

    Query Parameters

--- src/routes/markets.js
+++ src/routes/markets.js
@@ -2,13 +2,22 @@ const { Router } = require("express");
 const router = Router();
 
 const supermarkets = [
-  { store: "Whole Foods" },
-  { store: "Trafer Joes" },
-  { store: "Albertsons" },
+  { id: 1, store: "Whole Foods", miles: 0.6 },
+  { id: 2, store: "Trafer Joes", miles: 2.5 },
+  { id: 3, store: "Albertsons", miles: 2.8 },
+  { id: 4, store: "Trafer Joes", miles: 3.5 },
+  { id: 5, store: "Albertsons", miles: 1.8 },
 ];
 
 router.get("/", (req, res) => {
-  res.send(supermarkets);
+  const { miles } = req.query;
+  const parseMiles = parseInt(miles);
+  if (miles && !isNaN(parseMiles)) {
+    const filteredStores = supermarkets.filter((s) => s.miles <= parseMiles);
+    res.send(filteredStores);
+  } else {
+    res.send(supermarkets);
+  }
 });
 
 module.exports = router;
